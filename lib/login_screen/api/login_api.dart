3b48939d96cf921f18a53da738301868ee8e9e75
aprxrt3 <ajiprxty3@gmail.com>
2021-11-09 20:30:34 +0700

    refix api login

--- lib/login_screen/api/login_api.dart
+++ lib/login_screen/api/login_api.dart
@@ -2,11 +2,11 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../model/login_model.dart';
 
-class APIService {
+class loginAPI {
   Future<loginRespon> login(loginRequest requestModel) async {
-    String url = "http://172.104.44.22/api/method/login";
-
-    final response = await http.post(url, body: requestModel.toJson());
+    final response = await http.post(
+        Uri.parse("http://172.104.44.22/api/method/login"),
+        body: requestModel.toJson());
     if (response.statusCode == 200 || response.statusCode == 400) {
       return loginRespon.fromJson(
         json.decode(response.body),
